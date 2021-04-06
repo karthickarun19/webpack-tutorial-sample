fa15f55baded2efff1356db8b6d66d969dfda4ac
karthicksubramanian <crownkarthick@gmail.com>
2021-04-06 12:48:16 -0700

    "Added babel depedency and plugin"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-const heading = document.createElement('h1')
-heading.textContent = 'Hello World!'
-
-// Append heading node to the DOM
-const app = document.querySelector('#root')
-app.append(heading)
+class Game {
+    name = 'Violin Charades'
+  }
+  const myGame = new Game()
+  // Create paragraph node
+  const p = document.createElement('p')
+  p.textContent = `I like ${myGame.name}.`
+  
+  // Create heading node
+  const heading = document.createElement('h1')
+  heading.textContent = 'Interesting!'
+  
+  // Append SVG and heading nodes to the DOM
+  const app = document.querySelector('#root')
+  app.append(heading, p)
--- webpack.config.js
+++ webpack.config.js
@@ -18,5 +18,14 @@ module.exports = {
       inject : 'body',
     }),
 
-  ]
+  ],
+  module:{
+    rules:[{
+      test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+    }
+    ]
+  }
+  
 }
